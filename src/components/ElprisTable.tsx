dbfdd7db90b0b187ae108ff85ba40df6b5e6f5d0
Joel <joel1stenlund@gmail.com>
2025-10-31 17:04:19 +0100

    hopefully clears the tomorrow table at 01.00 :)

--- src/components/ElprisTable.tsx
+++ src/components/ElprisTable.tsx
@@ -116,7 +116,7 @@ const ElprisTable: React.FC<ElprisTableProps> = ({ tomorrow = false }) => {
         const nextFetchTime = new Date(thirteen.getTime() + 24 * 60 * 60 * 1000)
         const delay_13 = nextFetchTime.getTime() - Date.now()
         const midnight = new Date()
-        midnight.setHours(24, 0, 0, 0)
+        midnight.setHours(1, 0, 0, 0)
         const delay_midnight = midnight.getTime() - Date.now()
         setTimeout(clearTable, delay_midnight)
         setTimeout(fetchEveryMinuteUntilData, delay_13)
